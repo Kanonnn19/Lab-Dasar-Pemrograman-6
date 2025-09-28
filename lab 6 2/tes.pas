871a986398f76258e80d699cbeab4a2ff1683a91
Kanonnn19 <ahmadarrauf18@gmail.com>
2025-09-28 21:52:27 +0700

    Pertemuan 2 Lab 6

---
+++ lab 6 2/tes.pas
@@ -0,0 +1,49 @@
+uses crt;
+var 
+//  a, b, tambah, kurang, kali, modulo:integer;
+//  bagi:real;
+//  hasil : boolean;
+panjang, lebar,kali: integer;
+
+begin
+clrscr;
+writeln('masukkan panjang'); readln(panjang);
+writeln('masukkan lebar'); readln(lebar);
+
+kali := panjang *lebar;
+
+writeln('hasil kali : ',kali);
+// a:=10;
+// b:=3;
+
+// tambah := a + b;
+// kurang := a - b;
+// kali := a * b;
+// bagi :=a / b;
+// modulo :=a mod b;
+
+// writeln('hasil tambah : ', tambah);
+// writeln('hasil kurang : ', kurang);
+// writeln('hasil kali : ', kali);
+// writeln('hasil bagi : ', bagi:0:2);
+// writeln('hasil mod : ', modulo);
+// writeln(a = b);
+// writeln(a <> b);
+// writeln(a > b);
+// writeln(a < b);
+// writeln(a >= b);
+// writeln(a <= b);
+
+// writeln(true and true);
+// writeln(true and false);
+// writeln(false and true);
+// writeln(false and false);
+// writeln(true or false);
+// writeln(true or true);
+// writeln(false or true);
+// writeln(false or false);
+// writeln(not true);
+// writeln(not false);
+
+
+end.
