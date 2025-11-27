c8c4fe391c3cfae8f954447e7020593c330da70a
Kanonnn19 <ahmadarrauf18@gmail.com>
2025-11-27 11:25:14 +0700

    Lab 6
    Pertemuan 4

---
+++ Pertemuan 4/Program perloncatan.pas
@@ -0,0 +1,15 @@
+Program perloncatan;
+uses crt;
+label a, b, c, d, e;
+begin
+clrscr;
+    a:writeln('hello world');
+    goto b;
+    d:writeln('Fasilkom-TI');
+    goto e;
+    c:writeln('ilmu komputer');
+    goto d;
+    b:writeln('IKLC');
+    goto c;
+    e:
+end.
